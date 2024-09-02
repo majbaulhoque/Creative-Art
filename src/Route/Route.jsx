8faeff526908b07ce7b309e05661739634b3efc3
Majba UL Hoque <majba9399@gmail.com>
2024-09-02 15:25:12 +0600

    Basic Route setup

---
+++ src/Route/Route.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Pages/Home/Home";
+import MainLayout from "../Layout/MainLayout";
+
+const Route = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            }
+        ]
+    }
+])
+export default Route;
