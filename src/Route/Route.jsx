bcf5757303e1c7f817a928a284e96a93fd5f4cb6
mashuq0068 <mashuq0068@gmail.com>
2024-09-03 16:19:04 +0600

    implement routes in classes

--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -6,38 +6,67 @@ import AboutUs from "../Component/Home/AboutUs";
 import WhyUs from "../Component/Home/WhyUs";
 import OurVision from "../Component/Home/OurVision";
 import Curriculum from "../Component/Home/Curriculum.JSX";
-
+import AboutOurClasses from "../Component/Classes/AboutOurClasses";
+import FineArt from "../Component/Classes/FineArt";
+import VisualArt from "../Component/Classes/VisualArt";
+import SocialPresentation from "../Component/Classes/SocialPresentation";
+import AdultsDevelopment from "../Component/Classes/AdultsDevelopment";
+import DigitalArt from "../Component/Classes/DigitalArt";
 
 const Route = createBrowserRouter([
-    {
-        path: '/',
-        element: <MainLayout />,
-        children: [
-            {
-                path: '/',
-                element: <Home />,
-            },
-            {
-                path: '/about',
-                element: <AboutUs />,
-            },
-            {
-                path: '/whyUs',
-                element: <WhyUs />,
-            },
-            {
-                path: '/vision',
-                element: <OurVision />,
-            },
-            {
-                path: '/curriculum',
-                element: <Curriculum />,
-            },
-            {
-                path :"/classes",
-                element : <Classes />
-            }
-        ],
-    },
-])
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <AboutUs />,
+      },
+      {
+        path: "/whyUs",
+        element: <WhyUs />,
+      },
+      {
+        path: "/vision",
+        element: <OurVision />,
+      },
+      {
+        path: "/curriculum",
+        element: <Curriculum />,
+      },
+      {
+        path: "/classes",
+        element: <Classes />,
+      },
+      {
+        path: "/aboutOurClasses",
+        element: <AboutOurClasses />,
+      },
+      {
+        path: "/fineArt",
+        element: <FineArt />,
+      },
+      {
+        path: "/visualArt",
+        element: <VisualArt />,
+      },
+      {
+        path: "/social-presentation",
+        element: <SocialPresentation />,
+      },
+      {
+        path: "/digital-art",
+        element: <DigitalArt />,
+      },
+      {
+        path: "/adults-development",
+        element: <AdultsDevelopment />,
+      },
+    ],
+  },
+]);
 export default Route;
