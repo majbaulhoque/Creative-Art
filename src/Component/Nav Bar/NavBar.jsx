403b38cbb5f50331c5b0c4626cd7f5661b38951b
Majba UL Hoque <majba9399@gmail.com>
2024-09-05 17:32:28 +0600

    fixed contact us problem

--- src/Component/Nav Bar/NavBar.jsx
+++ src/Component/Nav Bar/NavBar.jsx
@@ -37,12 +37,12 @@ const NavBar = () => {
                                     onMouseEnter={() => handleMouseEnter('home')}
                                     onMouseLeave={handleMouseLeave}
                                 >
-                                    {/* <NavDropdown.Item as={Link} to="/">Home</NavDropdown.Item> */}
                                     <NavDropdown.Item as={Link} to="/about">About Us</NavDropdown.Item>
                                     <NavDropdown.Item as={Link} to="/whyUs">Why Us</NavDropdown.Item>
                                     <NavDropdown.Item as={Link} to="/vision">Our Vision</NavDropdown.Item>
                                     <NavDropdown.Item as={Link} to="/curriculum">Our Curriculum</NavDropdown.Item>
                                 </NavDropdown>
+
                                 <NavDropdown
                                     className="text-dark custom-dropdown text-decoration-none"
                                     title={
@@ -51,11 +51,9 @@ const NavBar = () => {
                                         </NavLink>
                                     }
                                     show={openDropdown === 'classes'}
-
                                     onMouseEnter={() => handleMouseEnter('classes')}
                                     onMouseLeave={handleMouseLeave}
                                 >
-                                    {/* <NavDropdown.Item as={Link} to="/classes">Classes</NavDropdown.Item> */}
                                     <NavDropdown.Item as={Link} to="/aboutOurClasses">About Our Classes</NavDropdown.Item>
                                     <NavDropdown.Item as={Link} to="/fineArt">Fine Art</NavDropdown.Item>
                                     <NavDropdown.Item as={Link} to="/visualArt">Visual Art</NavDropdown.Item>
@@ -64,14 +62,22 @@ const NavBar = () => {
                                     <NavDropdown.Item as={Link} to="/adults-development">Adults Development</NavDropdown.Item>
                                     <NavDropdown.Item as={Link} to="/art-for-senior">Art For Senior</NavDropdown.Item>
                                 </NavDropdown>
+
                                 <Nav.Link as={Link} to="/events" className='text-dark' onMouseEnter={handleMouseLeave}>
                                     Events
                                 </Nav.Link>
-                                {/* <Nav.Link as={Link} to="/products" className='text-dark' onMouseEnter={handleMouseLeave}>
-                                    Products
-                                </Nav.Link> */}
+
                                 <Nav.Link as={Link} to="/contact" className='text-dark' onMouseEnter={handleMouseLeave}>
                                     Contact Us
+                                </Nav.Link>
+
+                                {/* External links for social media */}
+                                <Nav.Link href="https://www.instagram.com/creativeartanddesigninc" target="_blank" rel="noopener noreferrer">
+                                    <img src='/Images/instagram.png' height={30} alt="Instagram" />
+                                </Nav.Link>
+
+                                <Nav.Link href="mailto:example@gmail.com" target="_blank" rel="noopener noreferrer">
+                                    <img src='/Images/gmail.png' height={30} alt="Gmail" />
                                 </Nav.Link>
                             </Nav>
                         </Navbar.Collapse>
